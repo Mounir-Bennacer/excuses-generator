179f5e6252b7f5456a144b742ca3e33ee759e6a3
Mounir Bennacer <benphaoris@gmail.com>
2021-08-01 23:33:02 +0100

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+
+const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+  console.log("MongoDB connected...");
+})
+.catch((err) => console.log(err));
+
+app.listen(8800, () => {
+  console.log('listening on port: 8800...')
+});
